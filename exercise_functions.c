29550ae44a0601f78e59c7a3ee686b3e6d6b338d
Viditgupta-official <viditgupta.official@gmail.com>
2024-05-30 16:06:15 +0530

    Nothing much

--- exercise_functions.c
+++ exercise_functions.c
@@ -1,15 +1,24 @@
 #include<stdio.h>
 
 void display();
+float sqr(float a);
 
 int main()
 {
     printf("\nOnly stupid people use C?");
 
     display();
+    sqr(float a);
+    printf("%f", a);
 }
 
 void display()
 {
-    printf("\nFools too use C!");
+    printf("\nFools too use C!\n");
+}
+
+float sqr(a)
+int a;
+{
+    return(a*a);
 }
